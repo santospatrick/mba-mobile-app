22c4e0a30a8f78fe3899d17ffce4f6b450c16c1a
Patrick Santos <patrickspteixeira@gmail.com>
2025-01-29 22:44:48 -0300

    feat: add input and form control

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,10 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { router } from 'expo-router'
+import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input'
+import { FormControl, FormControlLabel, FormControlLabelText } from '@/components/ui/form-control'
+import { VStack } from '@/components/ui/vstack'
+import { Mail } from 'lucide-react-native'
 
 type Props = {}
 
@@ -8,6 +12,22 @@ const Index = (props: Props) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Index</Text>
+      <VStack className="w-full max-w-[350px]">
+        <FormControl size="md">
+          <FormControlLabel>
+            <FormControlLabelText size="xs">E-MAIL</FormControlLabelText>
+          </FormControlLabel>
+          <Input className="my-1 border-l-0 border-t-0 border-r-0">
+            <InputSlot>
+              <InputIcon as={Mail} />
+            </InputSlot>
+            <InputField
+              type="text"
+              placeholder="mail@exemplo.br"
+            />
+          </Input>
+        </FormControl>
+      </VStack>
       <TouchableOpacity onPress={() => router.navigate('products')}>
         <Text>Go to homepage</Text>
       </TouchableOpacity>
